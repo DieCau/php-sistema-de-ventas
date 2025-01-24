1d681a84b253e1a604f13e9c4007495759f77b53
Diego Caucota <diegocaucota@gmail.com>
2025-01-23 23:56:21 -0300

    fix: roles index

--- roles/index.php
+++ roles/index.php
@@ -33,7 +33,7 @@ include ('../app/controllers/roles/listado_de_roles.php');
                 <div class="col-md-6">
                     <div class="card card-outline card-primary">
                         <div class="card-header">
-                            <h3 class="card-title">Roles registrado</h3>
+                            <h3 class="card-title">Roles registrados</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                                 </button>
