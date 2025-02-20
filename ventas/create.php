cd808fe1cda39f6dee26beac4e09d06a831837d6
Diego Caucota <diegocaucota@gmail.com>
2025-02-20 09:35:27 -0300

    fix: ventas card

--- ventas/create.php
+++ ventas/create.php
@@ -1,4 +1,127 @@
-<!-- Main de ventas -->
-<?php 
+<?php
+include('../app/config.php');
+include('../layout/sesion.php');
+
+include('../layout/parte1.php');
+
 
 ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-12">
+          <h1 class="m-0">Ventas</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-outline card-primary">
+            <div class="card-header">
+              <h3 class="card-title"><i class="fa fa-shopping-bag"></i> Venta Nº
+                <input type="text" value="1" style="text-align: center; border: none;" disabled>
+              </h3>
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+
+            </div>
+
+            <div class="card-body">
+              fdsafds
+            </div>
+
+          </div>
+
+        </div>
+
+        <div id="respuesta_create"></div>
+
+      </div>
+
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include('../layout/mensajes.php'); ?>
+<?php include('../layout/parte2.php'); ?>
+
+
+
+<script>
+  $(function() {
+    $("#example1").DataTable({
+      "pageLength": 5,
+      "language": {
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Productos",
+        "infoEmpty": "Mostrando 0 a 0 de 0 Productos",
+        "infoFiltered": "(Filtrado de _MAX_ total Productos)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Productos",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscador:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      "responsive": true,
+      "lengthChange": true,
+      "autoWidth": false,
+
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
+
+
+  $(function() {
+    $("#example2").DataTable({
+      "pageLength": 5,
+      "language": {
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Proveedores",
+        "infoEmpty": "Mostrando 0 a 0 de 0 Proveedores",
+        "infoFiltered": "(Filtrado de _MAX_ total Proveedores)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Proveedores",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscador:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+          "first": "Primero",
+          "last": "Ultimo",
+          "next": "Siguiente",
+          "previous": "Anterior"
+        }
+      },
+      "responsive": true,
+      "lengthChange": true,
+      "autoWidth": false,
+
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+  });
+</script>
